c614e00db071806533199e65f1c4dd35e52f25ef
David Ortinau <david.ortinau@microsoft.com>
2017-12-29 08:45:14 -0600

    naked button removes drop shadow on droid. styling for droid. Fixes #2

---
+++ Droid/Renderers/NakedButtonRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenSesame.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(Button), typeof(NakedButtonRenderer))] 
+namespace OpenSesame.Droid.Renderers
+{
+	public class NakedButtonRenderer : ButtonRenderer
+	{
+		protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<Button> e)
+		{
+			base.OnElementChanged(e);
+
+			Control.StateListAnimator = null; // removes shadow
+		}
+	}
+}
