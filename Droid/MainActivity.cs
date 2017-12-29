b316035449b40ed843d322e93ecccdc2b5b46998
David Ortinau <david.ortinau@microsoft.com>
2017-12-29 08:04:40 -0600

    helps to initialize the svg component. duh. Fixes #1

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,6 +7,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Droid;
+using FFImageLoading.Svg.Forms;
 
 namespace OpenSesame.Droid
 {
@@ -20,6 +22,8 @@ namespace OpenSesame.Droid
 
 			base.OnCreate(bundle);
 
+			CachedImageRenderer.Init(false);
+			var ignore = typeof(SvgCachedImage);
 			global::Xamarin.Forms.Forms.Init(this, bundle);
 
 
